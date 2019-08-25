2d2fdb454603996153bbcd7e8296514ee51b4957
mcceTest <zhuxy823@hotmail.com>
2019-08-24 22:35:42 -0400

    update

--- j1.js
+++ j1.js
@@ -1,2 +1,7 @@
 'use strict';
 
+let ele = document.getElementById("subTitle");
+
+ele.style.color = 'purple';
+
+setInterval(() => ele.style.visibility = (ele.style.visibility == 'hidden' ? 'visible' : 'hidden'), 1000);
