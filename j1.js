1b8b0487a7ec605d3858462b839cee8bcb9167b2
Xuyu Zhu <xuyu.zhu@tradeweb.com>
2019-08-26 18:34:28 -0400

    update

--- j1.js
+++ j1.js
@@ -4,3 +4,22 @@ let ele = document.getElementById("subTitle");
 
 setInterval(() => ele.style.visibility = (ele.style.visibility == 'hidden' ? 'visible' : 'hidden'), 1000);
 
+let tbl = document.getElementById("links");
+tbl.addEventListener("mouseover", function(event) {
+    event.target.style.background = 'pink';
+});
+
+tbl.addEventListener("mouseout", (event) => event.target.style.background='');
+
+
+
+
+function updateClock() {
+    let dt = new Date();
+    document.getElementById("hh").innerText = dt.getHours();
+    document.getElementById("mm").innerText = dt.getMinutes();
+    document.getElementById("ss").innerText = dt.getSeconds();
+}
+
+updateClock();
+setInterval(updateClock, 1000);
